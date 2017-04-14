8e75488eea9fcaffa57b6e32388c8dd5f3ccca1d
vei tsi <xtfkpi@gmail.com>
2017-04-14 17:05:28 +0300

    draws chances

--- parser.py
+++ parser.py
@@ -1,18 +1,13 @@
-fi = open("dev.txt","r")
-fo=open("dev.js","w")
-fo.write("var base=[]; base['dev']=[")
-for line in fi:
-        line = line[:-1]
-        print(line)
-        fo.write('"'+line+'",')
-fo.write("];")
-fi.close()
+import cards
 
-
-fi = open("qa.txt","r")
-fo.write("base['qa']=[")
-for line in fi:
-        line = line[:-1]
-        print(line)
-        fo.write('"'+line+'",')
+fo=open("base.js","w")
+fo.write("var base=[")
+for card in cards.base:
+        # print(card)
+        smiley = card['smiley'] if card['type']=='event' else ''
+        text=card['type']+" "+smiley+" "+card['en_title']+": "+card['en']
+        print(text)
+        fo.write('"'+text+'",')
 fo.write("];")
+fo.close()
+
